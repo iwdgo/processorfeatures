dec466dbb56d7f9b97afccfcdeb73c7441b145db
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2023-05-29 20:13:43 +0200

    Set line separators

---
+++ hwcap2flags_ppc64.go
@@ -0,0 +1,9 @@
+//go:build ppc64
+
+// Code generated using 'go generate'; DO NOT EDIT.
+// package buildref contains the the HWCAP flags for each architecture
+//
+//go:generate go run mkhwcap2flags.go
+package processorfeatures
+
+var AuxvFeatures = []ProcessorFeature{}
