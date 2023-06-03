9d96d53de165cf88f2dd5704bc17caf6d44fe59f
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2023-06-03 17:05:58 +0200

    Add test for load HWCAP2

---
+++ loadhwcap2_test.go
@@ -0,0 +1,15 @@
+package processorfeatures
+
+import (
+	"testing"
+)
+
+// LoadHWCAP2 returns HWCAP2 as a uint64 by reading /proc/self/auxv.
+// An error is returned otherwise. If the error is io.EOF, HWCAP2 is not found.
+func TestLoadHWCAP2(t *testing.T) {
+	v, err := LoadHWCAP2()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%b", v)
+}
